82a25996391d011162e616298c1f56cf17f6ddb2
code-by-mk <mohitsaini6997@gmail.com>
2024-09-12 12:36:31 +0530

    first commit for deploying

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,10 +4,9 @@ import { isAuthenticated } from "../middlewares/auth.js";
 import { catchAsyncErrors } from "../middlewares/catchAsyncError.js";
 
 const router = express.Router();
-
 router.post("/register", register);
 router.post('/verify-otp', Otpverification);
-router.post("/login",isAuthenticated, login);
+router.post("/login", login);
 router.get("/logout", isAuthenticated, logout);
 router.get("/getuser", isAuthenticated, getUser);
 
