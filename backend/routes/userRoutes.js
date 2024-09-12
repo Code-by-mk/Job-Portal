18f8d7bb66bf3964f97e024c112b8ec08db4a576
code-by-mk <mohitsaini6997@gmail.com>
2024-09-12 12:17:19 +0530

    first commit for deploying

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,12 +1,13 @@
 import express from "express";
 import { login, register, logout, getUser, Otpverification } from "../controllers/userController.js";
 import { isAuthenticated } from "../middlewares/auth.js";
+import { catchAsyncErrors } from "../middlewares/catchAsyncError.js";
 
 const router = express.Router();
 
 router.post("/register", register);
 router.post('/verify-otp', Otpverification);
-router.post("/login", login);
+router.post("/login",isAuthenticated, login);
 router.get("/logout", isAuthenticated, logout);
 router.get("/getuser", isAuthenticated, getUser);
 
